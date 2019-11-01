26fc3419b6a7978c59edae06d6b17a62ccddf497
mateusz7812 <mateusz78122@gmail.com>
2019-11-01 21:34:41 +0100

    Updates

---
+++ precache-manifest.f30ee4ca6f7d2beb6fcf34565612b517.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa8e271d7fc3f2a8fbbda737fab65891",
+    "url": "/lister-pwa-react/index.html"
+  },
+  {
+    "revision": "3e46eff18e368da08496",
+    "url": "/lister-pwa-react/static/css/main.8347de09.chunk.css"
+  },
+  {
+    "revision": "d9e8c30033004645c5be",
+    "url": "/lister-pwa-react/static/js/2.101a9016.chunk.js"
+  },
+  {
+    "revision": "3e46eff18e368da08496",
+    "url": "/lister-pwa-react/static/js/main.1b7324ab.chunk.js"
+  },
+  {
+    "revision": "2f92bd1c1e2859667a09",
+    "url": "/lister-pwa-react/static/js/runtime~main.a3447aff.js"
+  }
+]);
